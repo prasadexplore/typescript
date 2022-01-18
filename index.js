0ec4794f8c684e94fbe67560ae5eaf39da3b7840
Prasad Hewage <prasad@explorelogy.lk>
2022-01-19 04:42:04 +0530

    Catching Errors

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    var todo = response.data;
+    var Id = todo.Id;
+    var title = todo.Title;
+    var Finished = todo.Finished;
+    console.log("\n    The ID of :".concat(Id, "\n    Has a Title of :").concat(title, "\n    which is : ").concat(Finished, " .\n  "));
 });
--- index.ts
+++ index.ts
@@ -1,6 +1,26 @@
 import axios from 'axios';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+  The ID of :${id}
+  Has a Title of :${title}
+  is it Finished ?  : ${completed} .
+`);
+};
